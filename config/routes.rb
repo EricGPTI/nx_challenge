3413734cb73b01f4aa2f1445fa54d5de9ced8b8f
EricGPTI <ericgpti@gmail.com>
2021-07-31 23:55:51 -0300

    Criando rotas da aplicação

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :loans, only: [:show, :create] do
+    GET /loans/id, to: 'loans#show'
+    POST /loans, to:'loans#create'
   end
 end
